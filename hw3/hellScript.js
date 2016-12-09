cd03414b79e9b9d4bdeeeee6b113725ba2e45504
Kushnrom <kushnrom@gmail.com>
2016-12-09 14:55:08 +0300

    Really hard script i guess there are a lot of changes needed

---
+++ hw3/hellScript.js
@@ -0,0 +1,36 @@
+
+// 1st script from the home work
+var i;
+var a = '#';
+for (i = 0; i != 8; i++) 
+	{
+	console.log(a);
+	a =  a + "#";
+	}
+
+//2nd script from the home work 
+var i;
+var b = ' ';
+var c = '#';
+for (i = 0; i != 8; i++) 
+	{   
+		var a = 8;
+		var d = '';
+		while (a)
+		{
+        d = b + c + d
+		a--;
+		}	
+	console.log(d)
+		if (b == ' ')
+		{b = '#';
+		 c = ' ';
+		}
+		else 
+		{
+		b = ' ';
+		c = '#'
+		}	
+	}
+		
+	
